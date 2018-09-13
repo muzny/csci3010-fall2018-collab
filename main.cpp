3c82e8b2cee9a51f7baa6c90157176dbdd3b9ec7
Felix Muzny <muzny@colorado.edu>
2018-09-13 09:11:15 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// add your functions here. Make sure to include a function comment
+// specifying parameters and return values
+
+int main() {
+  // test your functions here
+  std::cout << "hello, here is some math: " << std::endl;
+  
+}
