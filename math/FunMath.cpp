bf2423285ef2c3560757118f74825fc7a4898713
Felix Muzny <muzny@colorado.edu>
2018-09-19 14:08:55 -0600

    update gitignore and add comments to NextLeapYear function

--- math/FunMath.cpp
+++ math/FunMath.cpp
@@ -1,10 +1,18 @@
 #include "FunMath.h"
 
+/**
+    This function calculates the next leap year. If the current year is a leap year,
+    this function returns the current year.
+    @param current_year the year after which to find the next leap year
+    @return int the next leap year that is >= current year
+*/
 int NextLeapYear(int current_year) {
     int curr = current_year;
     while (true) {
-        if ((curr % 4 == 0 && curr % 100 != 0) || (curr % 4 == 0 && curr % 100 == 0 && curr % 400 == 0)) {
-            return curr;
+        if (curr % 4 == 0) {
+            if ((curr % 100 != 0) || (curr % 400 == 0)) {
+                return curr;
+            }
         } 
         curr++;
     }
