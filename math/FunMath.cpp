7122e8dc172cafc4cd40ee12d2195fd85a0b0bbf
user <ho.xusong@gmail.com>
2018-09-20 10:26:34 -0600

    Added extra comments and test cases

--- math/FunMath.cpp
+++ math/FunMath.cpp
@@ -1,11 +1,11 @@
 #include "FunMath.h"
 
-/**
-    Adds all element from a that do not match with b
-*/
 std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b){
+//The return vector
     std::vector<int> c;
+//match boolean is set to false at the beginning and is set to true if a match is found
     bool match;
+//For loop essentially looks for matches that exist in both b and a
     for(int i = 0; i < a.size(); i++){
         match = false;
         for(int j = 0; j < b.size(); j++){
@@ -13,6 +13,7 @@ std::vector<int> MatchVectors(std::vector<int> a, std::vector<int> b){
                 match = true;
             }
         }
+//If a match is not found, then we keep the element of a 
         if(match == false){
             c.push_back(a[i]);
         }
--- math/test.cpp
+++ math/test.cpp
@@ -1,6 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-
 #include "FunMath.h" // include your header file here
 
 // Here is an example test case
@@ -10,3 +9,12 @@ TEST_CASE("plus operator on ints", "[plus]") {
 
 // Add more test cases down here
 
+TEST_CASE("Match function for vector matching", "[match]"){
+	std::vector<int> a = {1,2,3,4,5};
+	std::vector<int> b = {1,2,5};
+	std::vector<int> c = {};
+	REQUIRE (MatchVectors(a,b).size() == 2);
+	REQUIRE (MatchVectors(a,b)[0] == 3);
+	REQUIRE (MatchVectors(a,b)[1] == 4);
+	REQUIRE (MatchVectors(a,c).size() == 5);
+}
