d6b966e79aacb9727e7bddacf2bc652110336549
Josiah Graham <jogr9892@colorado.edu>
2018-09-20 10:41:47 -0600

    Added test cases

--- math/test.cpp
+++ math/test.cpp
@@ -10,3 +10,12 @@ TEST_CASE("plus operator on ints", "[plus]") {
 }
 
 // Add more test cases down here
+TEST_CASE("Even odd vectors computed", "[EvenOdd]") {
+    std::vector<int> even_odd_input1 = {1,2,5};
+    std::vector<int> even_odd_input2 = {56,7,53};
+    std::vector<int> even_odd_input3 = {0,5,76};
+
+    REQUIRE (EvenOddVectors(even_odd_input1) == std::vector<int> {1,0,1});
+    REQUIRE (EvenOddVectors(even_odd_input2) == std::vector<int> {0,1,1});
+    REQUIRE (EvenOddVectors(even_odd_input3) == std::vector<int> {0,1,0}); 
+}
