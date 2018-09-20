4c24c9df26d9f41e29108b9bd442c91d870e21e3
Thomas Young <thyo9470@colorado.edu>
2018-09-20 10:18:11 -0600

    Adding RemoveSubstring function

--- math/FunMath.cpp
+++ math/FunMath.cpp
@@ -1,1 +1,14 @@
 #include "FunMath.h"
+
+
+std::string RemoveSubstring(std::string s1, std::string s2){
+    int substring_start = -1;
+    substring_start = s1.find(s2); //find the index where the substring first occurs
+    s1.erase(s1.begin() + substring_start, s1.begin() + substring_start + s2.length()); //erase the substring
+    if(substring_start != -1){
+        return s1;
+    }
+    else{
+        return "not found";
+    }
+}
